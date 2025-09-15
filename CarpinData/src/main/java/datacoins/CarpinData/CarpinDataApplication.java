cafb8ee0ca0593c859b238cd7be05893fda902b1
Saul de Jesus Sical Yoc <Ssical-2024422@kinal.edu.gt>
2025-09-15 17:42:55 -0600

    Add files via upload

---
+++ CarpinData/src/main/java/datacoins/CarpinData/CarpinDataApplication.java
@@ -0,0 +1,13 @@
+package datacoins.CarpinData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpinDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpinDataApplication.class, args);
+	}
+
+}
---
+++ CarpinData/src/test/java/datacoins/CarpinData/CarpinDataApplicationTests.java
@@ -0,0 +1,13 @@
+package datacoins.CarpinData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpinDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
